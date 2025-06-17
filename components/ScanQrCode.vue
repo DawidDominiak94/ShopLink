6592dfd5f1884609ba113b71f88f3a87ffa8e1e6
Dawid Dominiak <dawid.dominiak.94@gmail.com>
2025-06-17 21:34:05 +0200

    commit

--- components/ScanQrCode.vue
+++ components/ScanQrCode.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="flex flex-col gap-4 text-center">
+    {{ test }}
     <qrcode-stream @detect="onDetect"></qrcode-stream>
   </div>
 </template>
 
 <script lang="ts" setup>
   import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
+  const test = ref();
 
   function onDetect (detectedCodes : any) 
   {
-    console.log(detectedCodes);
+    test.value = detectedCodes;
   }
 </script>
 
