1c36563cfc08e3a8e533e39faa5fc8afc7d42084
Dawid Dominiak <dawid.dominiak.94@gmail.com>
2025-06-17 21:29:52 +0200

    qr code

--- components/ScanQrCode.vue
+++ components/ScanQrCode.vue
@@ -1,49 +1,16 @@
 <template>
   <div class="flex flex-col gap-4 text-center">
-    <div>
-      <button @click="enabled = !enabled">
-        {{ enabled ? 'Stop' : 'Start' }}
-      </button>
-    </div>
-
-    <div>
-      <div
-        v-for="camera of cameras"
-        :key="camera.deviceId"
-        class="px-2 py-1 cursor-pointer"
-        :class="{ 'text-primary': currentCamera === camera.deviceId }"
-        @click="currentCamera = camera.deviceId"
-      >
-        {{ camera.label }}
-      </div>
-    </div>
-    <div>
-      <video ref="video" muted autoplay controls class="h-100 w-auto" />
-    </div>
+    <qrcode-stream @detect="onDetect"></qrcode-stream>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { useDevicesList, useUserMedia } from '@vueuse/core';
+  import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
 
-  const currentCamera = shallowRef<string>()
-  const { videoInputs: cameras } = useDevicesList({
-    requestPermissions: true,
-    onUpdated() {
-      if (!cameras.value.find(i => i.deviceId === currentCamera.value))
-        currentCamera.value = cameras.value[0]?.deviceId
-    },
-  })
-
-  const video = useTemplateRef<HTMLVideoElement>('video')
-  const { stream, enabled } = useUserMedia({
-    constraints: reactive({ video: { deviceId: currentCamera } }),
-  })
-
-  watchEffect(() => {
-    if (video.value)
-      video.value.srcObject = stream.value!
-  })
+  function onDetect (detectedCodes : any) 
+  {
+    console.log(detectedCodes);
+  }
 </script>
 
 <style>
